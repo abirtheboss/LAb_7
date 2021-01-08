d0a9cb9857bf245bf3e2a981f6a8d8b30dbf9189
abirtheboss <72722459+abirtheboss@users.noreply.github.com>
2021-01-07 22:02:07 -0800

    Lab task 7

---
+++ Question 1/interface(1)/interface(1)/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace interface_1_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BasicCalculator basicCalculator = new BasicCalculator();
+
+            Console.WriteLine(basicCalculator.sum(5, 5));
+            Console.WriteLine(basicCalculator.sub(10, 10));
+            Console.WriteLine(basicCalculator.multiplication(5, 5));
+            Console.WriteLine(basicCalculator.division(10, 10));
+
+            ScientificCalculator scientificCalculator = new ScientificCalculator();
+
+            Console.WriteLine(scientificCalculator.root(64));
+            Console.WriteLine(scientificCalculator.square(4));
+            Console.WriteLine(scientificCalculator.sub(10, 15));
+            Console.WriteLine(scientificCalculator.multiplication(5, 5));
+
+
+        }
+    }
+}
